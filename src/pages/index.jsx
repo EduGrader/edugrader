80c3e5a6634eed69cceb4bfae3b99a6553e253ba
antpoo <1jiangand2@gmail.com>
2023-10-20 19:26:40 -0400

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,11 +8,15 @@ export default function Home() {
         <main
             className={`flex min-h-screen flex-col items-center justify-between`}
         >
-            <div className="bg-sky-400 h-[550px] w-full flex justify-center content-center flex-wrap flex-col">
+            <div className="bg-primary h-[550px] w-full flex justify-center content-center flex-wrap flex-col">
                 <h1 className="text-white text-7xl">EduGrader</h1>
-                <button className="text-white rounded-sm bg-cyan-800">
+                <button className="text-white bg-green-500 rounded-sm">
                     Get Started
                 </button>
+            </div>
+            <div className="w-[600px] flex flex-col flex-wrap justify-center items-center space-y-10 text-center">
+                <h1 className="text-5xl">A simple and intuitive way to grade</h1>
+                <h2 className="text-lg">EduGrader takes all the features you need and packs them in an intuitive and easy-to-use environment.</h2>
             </div>
         </main>
     );
